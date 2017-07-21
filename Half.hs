c93eaefb13e59a91475a4485995b72887178b28f
Ryosuke Kamesawa <emak.ryo@gmail.com>
2017-07-21 15:32:28 +0900

    Refactor

--- Half.hs
+++ Half.hs
@@ -12,13 +12,15 @@ inf = 1/0
 -- NB. non-synthesizable
 toFloating :: Floating f => Half -> f
 toFloating (sign, expo, frac)
-    | e == 0 = (-1)**b * 2**(-24) * f
-    | b == 0 && e == 31 && f == 0 = inf
-    | b == 1 && e == 31 && f == 0 = -inf
-    | e == 31 = nan
-    | otherwise = (-1)**b * 2**(e-25) * (f + 2**10)
+    | e == 0 = (-1)**s * 2**(1-fwidth-bias) * f
+    | e == mb && f == 0 = (-1)**s * inf
+    | e == mb = nan
+    | otherwise = (-1)**s * 2**(e-fwidth-bias) * (f + 2**fwidth)
     where
-    b = fromIntegral $ sign
+    mb = maxBound :: Unsigned 5
+    bias = fromIntegral $ shiftR mb 1
+    fwidth = 10
+    s = fromIntegral $ sign
     e = fromIntegral $ expo
     f = fromIntegral $ frac
 
@@ -26,14 +28,17 @@ toFloating (sign, expo, frac)
 toHalf :: (Floating a, RealFrac a, RealFloat a) => a -> Half
 toHalf x = (s, e, f)
     where
+    mb = maxBound :: Unsigned 5
+    bias = fromIntegral $ shiftR mb 1
+    fwidth = 10
     s = if x >= 0 then 0 else 1 :: Bit
-    e' = (floor $ logBase 2 $ abs x) :: Integer
-    e | x==0 = 0
-      | isInfinite x || isNaN x = 31
-      | otherwise = (fromInteger $ max (-15) $ min 16 e')+15
-    f | e==31 || isInfinite x = 0
+    e' = fromIntegral $ floor $ logBase 2 $ abs x
+    e | x==0 || e' < (-bias) = 0
+      | isInfinite x || isNaN x || e' > (fromIntegral $ mb-bias) = mb
+      | otherwise = e'+bias
+    f | e==maxBound || isInfinite x = 0
       | isNaN x = 1
-      | otherwise = floor $ (abs x / (2**fromIntegral e')-1) * 2**10
+      | otherwise = floor $ (abs x / (2**e')-1) * 2**fwidth
 
 -- 3 clock latency
 fromSigned :: Signal (Signed 16) -> Signal Half
@@ -45,111 +50,106 @@ fromSigned x = bundle (s3, e3, f3)
     s2 = register 0 s1
     abs2 = register 0 abs1
     lz2 = register 0 $ (fromIntegral . countLeadingZeros) <$> abs1 :: Signal (Unsigned 5)
-    lz2' = fromIntegral <$> lz2
-    e2 = mux (abs2 .==. 0) (signal 0) (30-lz2)
-    f2 = shift <$> abs2 <*> (lz2'-5)
 
     s3 = register 0 $ s2
-    e3 = register 0 $ e2
-    f3 = register 0 $ resize <$> f2 :: Signal (Unsigned 10)
+    e3 = register 0 $ mux (abs2 .==. 0) 0 (30-lz2)
+    lz' = fromIntegral <$> lz2
+    f' = shift <$> abs2 <*> (lz'-5)
+    f3 = register 0 $ resize <$> f' :: Signal (Unsigned 10)
+
+fromSignedTest xs = sampleN (L.length xs + 3) $ simulate fromSigned $ xs
 
 concat# :: KnownNat n => KnownNat m => Unsigned n -> Unsigned m -> Unsigned (n+m)
 concat# x y = unpack $ (pack x) ++# (pack y)
 
 -- 3 clock latency
 add# :: Signal Half -> Signal Half -> Signal Half
-add# a b = add3 $ add2 $ add1 a b
+add# a b = f3 $ f2 $ f1 a b
+    where
+    f1 a b = register (unpack 0) $ add1 <$> a <*> b
+    f2 x = register (unpack 0) $ add2 <$> x
+    f3 x = register (unpack 0) $ add3 <$> x
 
 type StAdd1 = (Bit, Bit, Unsigned 5, Unsigned 10, Unsigned 10, Unsigned 5)
-add1 :: Signal Half -> Signal Half -> Signal StAdd1
-add1 a b = bundle (op1, s1, ea1, fa1, fb1, de1)
+add1 :: Half -> Half -> StAdd1
+add1 a b = (op1, s1, ea1, fa1, fb1, de1)
     where
-    (sa, ea, fa) = unbundle a
-    (sb, eb, fb) = unbundle b
-    op = xor <$> sa <*> sb -- 0: add, 1: sub
-    a' = concat# <$> ea <*> fa
-    b' = concat# <$> eb <*> fb
-    cmp = a' .>. b'
-    op1 = register 0 op
-    s1 = register 0 $ mux (op .==. 0) sa (mux cmp sa sb)
-    ea1 = register 0 $ mux cmp ea eb
-    fa1 = register 0 $ mux cmp fa fb
-    fb1 = register 0 $ mux cmp fb fa
-    de1 = register 0 $ mux cmp (ea-eb) (eb-ea)
+    (sa, ea, fa) = a
+    (sb, eb, fb) = b
+    op1 = xor sa sb
+    cmp = (concat# ea fa) > (concat# eb fb)
+    s1 | op1 == 0 = sa
+       | cmp = sa
+       | otherwise = sb
+    (ea1, fa1, fb1, de1)
+        | cmp = (ea, fa, fb, ea-eb)
+        | otherwise = (eb, fb, fa, eb-ea)
 
 type StAdd2 = (Bit, Unsigned 5, Unsigned 5, Unsigned 12)
-add2 :: Signal StAdd1 -> Signal StAdd2
-add2 x = bundle (s2, e2, lz2, f2)
+add2 :: StAdd1 -> StAdd2
+add2 (op1, s1, ea1, fa1, fb1, de1) = (s1, e2, lz2, f2)
     where
-    (op1, s1, ea1, fa1, fb1, de1) = unbundle x
-    one = 1 :: BitVector 2
-    fa' = (\x -> unpack $ one ++# (pack x)) <$> fa1
-    fb' = (\x y-> unpack $ shiftR (one ++# pack x) (fromIntegral y)) <$> fb1 <*> de1
-    f' = mux (op1 .==. 0) (fa'+fb') (fa'-fb') :: Signal (Unsigned 12)
-    lz = (fromIntegral . countLeadingZeros) <$> f'
-    s2 = register 0 s1
-    e2 = register 0 $ mux (ea1 .==. 0) 0 (ea1-lz+1)
-    lz2 = register 0 lz
-    f2 = register 0 f'
+    extOne x = unpack $ (1 ::BitVector 2) ++# pack x
+    fa' = extOne fa1
+    fb' = shiftR (extOne fb1) (fromIntegral de1)
+    f2 = if op1 == 0 then fa'+fb' else fa'-fb' :: Unsigned 12
+    lz2 = fromIntegral $ countLeadingZeros f2
+    e2 = if ea1 == 0 then 0 else ea1-lz2+1
 
-add3 :: Signal StAdd2 -> Signal Half
-add3 x = bundle (s3, e3, f3)
+add3 :: StAdd2 -> Half
+add3 (s2, e2, lz2, f2) = (s2, e3, f3)
     where
-    (s2, e2, lz2, f2) = unbundle x
-    f''p = (\x y -> shift x $ fromIntegral (y - 1)) <$> f2 <*> lz2
-    f''0 = (\x -> shiftR x 1) <$> f2
-    f'' = mux (lz2 .==. 0) f''0 f''p
-    s3 = register 0 s2
-    e3 = register 0 $ mux (f2 .==. 0) 0 e2
-    f3 = register 0 $ (unpack . slice d9 d0) <$> f''
+    fp = shift f2 $ fromIntegral $ lz2-1
+    f0 = shiftR f2 1
+    f' = if lz2 == 0 then f0 else fp
+    e3 = if f2 == 0 then 0 else e2
+    f3 = unpack $ slice d9 d0 f'
 
 -- 3 clock latency
 mul# :: Signal Half -> Signal Half -> Signal Half
-mul# a b = mul3 $ mul2 $ mul1 a b
+mul# a b = f3 $ f2 $ f1 a b
+    where
+    f1 a b = register (0, True, 0, 0) $ mul1 <$> a <*> b
+    f2 x = register (0, True, False, False, 0, 0) $ mul2 <$> x
+    f3 x = register (0, 0, 0) $ mul3 <$> x
 
 type StMul1 = (Bit, Bool, Unsigned 6, Unsigned 11)
-mul1 :: Signal Half -> Signal Half -> Signal StMul1
-mul1 a b = bundle (s1, ez1, e1, f1)
+mul1 :: Half -> Half -> StMul1
+mul1 a b = (s, ez, e, f)
     where
-    (sa, ea, fa) = unbundle a
-    (sb, eb, fb) = unbundle b
-    s1 = register 0 $ pack <$> (xor <$> sa <*> sb)
-    ez1 = register True $ (ea .==. 0) .||. (eb .==. 0)
-    e1 = register 0 $ plus <$> ea <*> eb
     extOne x = unpack $ (1 :: Bit) ++# (pack x)
-    fa' = extOne <$> fa :: Signal (Unsigned 11)
-    fb' = extOne <$> fb :: Signal (Unsigned 11)
-    f' = times <$> fa' <*> fb' :: Signal (Unsigned 22)
-    f1 = register 0 $ (unpack . slice d21 d11) <$> f'
+    (sa, ea, fa) = a
+    (sb, eb, fb) = b
+    s = xor sa sb
+    ez = (ea == 0) || (eb == 0)
+    e = plus ea eb
+    fa' = extOne fa :: Unsigned 11
+    fb' = extOne fb :: Unsigned 11
+    f' = times fa' fb' :: Unsigned 22
+    f = unpack $ slice d21 d11 f'
 
-type StMul2 = (Bit, Bool, Bool, Unsigned 5, Unsigned 10)
-mul2 :: Signal StMul1 -> Signal StMul2
-mul2 x = bundle (s2, ez2, c2, e2, f2)
+type StMul2 = (Bit, Bool, Bool, Bool, Unsigned 5, Unsigned 10)
+mul2 :: StMul1 -> StMul2
+mul2 (s1, ez1, e1, f1) = (s1, ez1, c2, ov2, e2, f2)
     where
-    (s1, ez1, e1, f1) = unbundle x
-    s2 = register 0 s1
-    ez2 = register True ez1
     e' = e1-15
-    e2 = mux ((msb <$> e') .==. 0) (resize <$> e') 31
-    c = (\x -> (msb x) == 1) <$> f1
-    c2 = register True c
-    f' = (\x -> shift x 1) <$> f1
-    f'' = mux c f1 f'
-    f2 = register 0 $ (unpack . slice d9 d0) <$> f''
+    ov2 = msb e' == 1
+    e2 = resize e'
+    c2 = msb f1 == 1
+    f' = if c2 then f1 else shift f1 1
+    f2 = unpack $ slice d9 d0 f'
 
-mul3 :: Signal StMul2 -> Signal Half
-mul3 x = bundle (s3, e3, f3)
+mul3 :: StMul2 -> Half
+mul3 (s2, ez2, c2, ov2, e2, f2) = (s2, e3, f3)
     where
-    (s2, ez2, c2, e2, f2) = unbundle x
-    s3 = register 0 s2
-    e' = mux c2 (e2+1) e2
-    e'' = mux ez2 0 e'
-    e3 = register 0 e''
-    f3 = register 0 $ mux ez2 0 f2
+    e3 | ez2 = 0
+       | ov2 = 31
+       | c2 = e2+1
+       | otherwise = e2
+    f3 | ez2 || ov2 = 0
+       | otherwise = f2
 
-test1 unop xs = sampleN (L.length xs) $ simulate unop $ toHalf <$> xs
-
-test2 binop (xs::[Double]) (ys::[Double]) = zs
+binOpTest binop (xs::[Double]) (ys::[Double]) = zs
     where
     binop' (x :: Signal (BitVector 32)) =
         let (a, b) = unbundle (split <$> x)
